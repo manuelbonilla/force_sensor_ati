c5a56a4646c64658315f788aeae0abef920904ea
Manuel Bonilla <josemanuelbonilla@gmail.com>
2016-05-17 13:25:34 +0200

    Now it is working for any number of topics

--- src/merge_forces.cpp
+++ src/merge_forces.cpp
@@ -6,15 +6,11 @@
 #include <string>
 
 std_msgs::Float64MultiArray leftforces, rightforces;
+std::vector<std_msgs::Float64MultiArray> vec_allforces;
 
-void callbackleft(const std_msgs::Float64MultiArray::ConstPtr& msg)
+void callbackforce(const std_msgs::Float64MultiArray::ConstPtr& msg, int i)
 {
-    leftforces.data = msg->data;
-}
-
-void callbackright(const std_msgs::Float64MultiArray::ConstPtr& msg)
-{
-    rightforces.data = msg->data;
+    vec_allforces[i].data = msg->data;
 }
 
 int main(int argc, char **argv)
@@ -29,16 +25,35 @@ int main(int argc, char **argv)
 
     ros::Rate rate(spin_rate);
 
-    ros::Subscriber sub_leftforces = nh.subscribe("/left/allforces", 1, callbackleft);
-    ros::Subscriber sub_rightforces = nh.subscribe("/right/allforces", 1, callbackright);
-
     ros::Publisher pub_allforces = nh.advertise<std_msgs::Float64MultiArray>( "allforces", 1 );
+
+    XmlRpc::XmlRpcValue my_list;
+    nh.getParam("topics", my_list);
+    ROS_ASSERT(my_list.getType() == XmlRpc::XmlRpcValue::TypeArray);
+
+    std::vector<ros::Subscriber> list_subs;
+    for ( int i = 0; i < my_list.size(); ++i)
+    {
+        ROS_ASSERT(my_list[i].getType() == XmlRpc::XmlRpcValue::TypeString);
+        ROS_INFO_STREAM("Mering topic: " <<  static_cast<std::string>(my_list[i]).c_str());
+        std_msgs::Float64MultiArray new_forces;
+        vec_allforces.push_back( new_forces );
+        list_subs.push_back( nh.subscribe<std_msgs::Float64MultiArray>( static_cast<std::string>(my_list[i]).c_str(), 1, boost::bind(&callbackforce, _1, i) ) );
+
+    }
 
     while ( nh.ok() )
     {
         std_msgs::Float64MultiArray allforces;
-        allforces.data = leftforces.data;
-        allforces.data.insert(allforces.data.end(), rightforces.data.begin(), rightforces.data.end());
+        if (vec_allforces.size() > 0)
+        {
+            allforces.data = vec_allforces[0].data;
+            for (unsigned int i = 1; i < vec_allforces.size(); ++i)
+            {
+                allforces.data.insert(allforces.data.end(), vec_allforces[i].data.begin(), vec_allforces[i].data.end());
+            }
+        }
+
         pub_allforces.publish(allforces);
         ros::spinOnce();
         rate.sleep();
